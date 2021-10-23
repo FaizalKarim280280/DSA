2dbe67fa26b1bfdfc32b0c6066b8c4f0aafc9748
Faizal Karim <faizalkarim280@yahoo.com>
2021-10-23 15:14:35 +0530

    First part of Q4

--- Assignment-3/Q4.c
+++ Assignment-3/Q4.c
@@ -1,4 +1,87 @@
-//
-// Created by faizal on 20-10-2021.
-//
+#include <stdio.h>
 
+#define MAX 1000000
+
+
+void printArray(int a[],int n){
+    for(int i = 0;i<n;i++)
+        printf("%d ",a[i]);
+}
+
+void insertItemAtLast(int a[],int n, int key){
+    a[n] = key;
+}
+void insertItemAtFirst(int a[],int n,int key){
+    for(int i = n;i>0;i--)
+        a[i] = a[i-1];
+    a[0] = key;
+}
+void insertItemAtIndex(int a[],int n,int idx,int key){
+    for(int i = n;i>idx;i--)
+        a[i] = a[i-1];
+    a[idx] = key;
+}
+void deleteItemFromLast(int a[],int *n){
+    -- *n;
+}
+void deleteItemFromFirst(int a[],int n){
+    for(int i = 1;i<n;i++)
+        a[i-1] = a[i];
+}
+void deleteItemFromIndex(int a[],int n,int idx){
+    for(int i = idx;i<n-1;i++)
+        a[i] = a[i+1];
+}
+int FindItemUnsorted(int a[],int n){
+    for(int i = 0;i<n;i++)
+        if(a[i+1]<a[i])
+            return i+1;
+    return -1;
+}
+int FindItemSorted(int a[],int n){
+    for(int i=0;i<n;i++)
+        if(a[i+1]<a[i])
+            return i;
+    return -1;
+}
+void sortArray(int a[],int n){
+    for(int i = 1;i<n;i++)
+    {
+        for(int j = 0;j<n-i;j++)
+        {
+            if(a[j] > a[j+1])
+            {
+                int temp = a[j+1];
+                a[j+1] = a[j];
+                a[j] = temp;
+            }
+        }
+    }
+}
+void extractSubset(int a[],int n,int x,int y){
+    for(int i = x;i<y;i++)
+        printf("%d ",a[i]);
+}
+void deleteSubset(int a[],int n,int x,int y){
+    for(int i = x;i<n;i++)
+    {
+        if(i + y>=n)
+            break;
+        a[i] = a[i+y];
+    }
+}
+void splitArray(int a[],int n){
+    printArray(a,n);
+}
+void cloneArray(int a[],int n,int b[]){
+    for(int i = n; i<2*n;i++)
+        a[i] = b[i-n];
+}
+
+
+int main(){
+
+    int a[MAX] = {0};
+
+    return 0;
+}
